615c251536b1486a0272e5a7ada65891f5c897cb
David W. Lotts <david.lotts@parsons.com>
2016-08-18 13:30:43 -0400

    split methods into server and test.

--- dao/mongodb.rya/src/main/java/mvm/rya/mongodb/MongoConnectorFactory.java
+++ dao/mongodb.rya/src/main/java/mvm/rya/mongodb/MongoConnectorFactory.java
@@ -43,55 +43,75 @@ import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
 public class MongoConnectorFactory {
     private static MongoClient mongoClient;
 
+    private final static String MSG_INTRO = "Failed to connect to MongoDB: ";
+
     /**
      * @param conf The {@link Configuration} defining how to construct the MongoClient.
      * @return A {@link MongoClient}.  This client is lazy loaded and the same one
      * is used throughout the lifecycle of the application.
      * @throws IOException - if MongodForTestsFactory constructor has an io exception.
      * @throws ConfigurationRuntimeException - Thrown if the configured server, port, user, or others are missing.
-     * @throws UnknownHostException - if MongoDB host name is not found.
+     * @throws MongoException  if can't connect despite conf parameters are given
      */
     public static synchronized MongoClient getMongoClient(final Configuration conf)
             throws ConfigurationRuntimeException, MongoException {
         if (mongoClient == null) {
             // The static client has not yet created, is it a test/mock instance, or a service?
             if (conf.getBoolean(MongoDBRdfConfiguration.USE_TEST_MONGO, false)) {
-                try {
-                    MongodForTestsFactory testsFactory = MongodForTestsFactory.with(Version.Main.PRODUCTION);
-                    mongoClient = testsFactory.newMongo();
-                } catch (IOException e) {
-                    // Rethrow as an unchecked error.  Since we are in a test mode here, just fail fast.
-                    throw new MongoException("While creating a factory for a test/mock MongoDB instance.",e);
-                }
+                createMongoClientForTests();
             } else {
-                // Connect to a running Mongo server
-                final String host = requireNonNull(conf.get(MongoDBRdfConfiguration.MONGO_INSTANCE), "MongoDB host name is required");
-                final int port = requireNonNullInt(conf.get(MongoDBRdfConfiguration.MONGO_INSTANCE_PORT),
-                        "MongoDB Port number is required.");
-                ServerAddress server;
-                try {
-                    server = new ServerAddress(host, port);
-                } catch (UnknownHostException e) {
-                    throw new MongoException("Connecting MongoDB instance, cannot find host="+host,e);
-                }
-                // check for authentication credentials
-                if (conf.get(MongoDBRdfConfiguration.MONGO_USER) != null) {
-                    final String username = conf.get(MongoDBRdfConfiguration.MONGO_USER);
-                    final String dbName = requireNonNull(conf.get(MongoDBRdfConfiguration.MONGO_DB_NAME),
-                            MongoDBRdfConfiguration.MONGO_DB_NAME + " is null but required configuration if "
-                                    + MongoDBRdfConfiguration.MONGO_USER + " is configured.");
-                    final char[] pswd = requireNonNull(conf.get(MongoDBRdfConfiguration.MONGO_USER_PASSWORD),
-                            MongoDBRdfConfiguration.MONGO_USER_PASSWORD + " is null but required configuration if "
-                                    + MongoDBRdfConfiguration.MONGO_USER + " is configured.").toCharArray();
-                    final MongoCredential cred = MongoCredential.createCredential(username, dbName, pswd);
-                    mongoClient = new MongoClient(server, Arrays.asList(cred));
-                } else {
-                    // No user was configured:
-                    mongoClient = new MongoClient(server);
-                }
+                createMongoClientForServer(conf);
             }
         }
         return mongoClient;
+    }
+
+    /**
+     * Create a local temporary MongoDB instance and client object and assign it to this class's static mongoClient 
+     * @throws MongoException  if can't connect
+     */
+    private static void createMongoClientForTests() throws MongoException {
+        try {
+            MongodForTestsFactory testsFactory = MongodForTestsFactory.with(Version.Main.PRODUCTION);
+            mongoClient = testsFactory.newMongo();
+        } catch (IOException e) {
+            // Rethrow as an unchecked error.  Since we are in a test mode here, just fail fast.
+            throw new MongoException(MSG_INTRO+"creating a factory for a test/mock MongoDB instance.",e);
+        }
+    }
+
+    /**
+     * Create a MongoDB client object and assign it to this class's static mongoClient 
+     * @param conf configuration containing connection parameters
+     * @throws ConfigurationRuntimeException - Thrown if the configured server, port, user, or others are missing.
+     * @throws MongoException  if can't connect despite conf parameters are given
+     */
+    private static void createMongoClientForServer(final Configuration conf)
+            throws ConfigurationRuntimeException, MongoException {
+        // Connect to a running Mongo server
+        final String host = requireNonNull(conf.get(MongoDBRdfConfiguration.MONGO_INSTANCE), MSG_INTRO+"host name is required");
+        final int port = requireNonNullInt(conf.get(MongoDBRdfConfiguration.MONGO_INSTANCE_PORT), MSG_INTRO+"Port number is required.");
+        ServerAddress server;
+        try {
+            server = new ServerAddress(host, port);
+        } catch (UnknownHostException e) {
+            throw new MongoException(MSG_INTRO + "cannot find host="+host,e);
+        }
+        // check for authentication credentials
+        if (conf.get(MongoDBRdfConfiguration.MONGO_USER) != null) {
+            final String username = conf.get(MongoDBRdfConfiguration.MONGO_USER);
+            final String dbName = requireNonNull(conf.get(MongoDBRdfConfiguration.MONGO_DB_NAME),
+                    MSG_INTRO + MongoDBRdfConfiguration.MONGO_DB_NAME + " is null but required configuration if "
+                            + MongoDBRdfConfiguration.MONGO_USER + " is configured.");
+            final char[] pswd = requireNonNull(conf.get(MongoDBRdfConfiguration.MONGO_USER_PASSWORD),
+                    MSG_INTRO + MongoDBRdfConfiguration.MONGO_USER_PASSWORD + " is null but required configuration if "
+                            + MongoDBRdfConfiguration.MONGO_USER + " is configured.").toCharArray();
+            final MongoCredential cred = MongoCredential.createCredential(username, dbName, pswd);
+            mongoClient = new MongoClient(server, Arrays.asList(cred));
+        } else {
+            // No user was configured:
+            mongoClient = new MongoClient(server);
+        }
     }
 
     /**
